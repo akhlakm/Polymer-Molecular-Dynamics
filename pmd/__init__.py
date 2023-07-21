fa1cd136c55a394ee51e99f396b8547e55dec332
akhlakm <me@akhlakm.com>
2023-07-21 16:52:40 -0400

    Add OpenMol builder

--- pmd/__init__.py
+++ pmd/__init__.py
@@ -3,7 +3,7 @@ import sys
 from .core import (EMC, NPT, NVT, PSP, Equilibration, HeatFluxMeasurement,
                    Lammps, Minimization, MSDMeasurement, Pmd, RgMeasurement,
                    ShearDeformation, Slurm, SolventSystem, System,
-                   TensileDeformation, TgMeasurement, Torque)
+                   TensileDeformation, TgMeasurement, Torque, OpenMol)
 from .postprocessing import (calculate_diffusivity, calculate_Tg,
                              read_lammpstrj, read_lammpstrj_by_type)
 
@@ -24,5 +24,6 @@ __all__ = [
     Lammps, Pmd, Torque, Slurm, System, SolventSystem, EMC, PSP, Minimization,
     Equilibration, RgMeasurement, TgMeasurement, MSDMeasurement,
     TensileDeformation, ShearDeformation, HeatFluxMeasurement, NVT, NPT,
-    calculate_diffusivity, calculate_Tg, read_lammpstrj, read_lammpstrj_by_type
+    calculate_diffusivity, calculate_Tg, read_lammpstrj, read_lammpstrj_by_type,
+    OpenMol
 ]
--- pmd/core/__init__.py
+++ pmd/core/__init__.py
@@ -1,4 +1,4 @@
-from .Builder import EMC, PSP
+from .Builder import EMC, PSP, OpenMol
 from .Job import Slurm, Torque
 from .Lammps import Lammps
 from .Pmd import Pmd
@@ -10,5 +10,6 @@ from .System import SolventSystem, System
 __all__ = [
     System, SolventSystem, Lammps, Torque, Slurm, Pmd, EMC, PSP, Minimization,
     Equilibration, TgMeasurement, MSDMeasurement, RgMeasurement,
-    TensileDeformation, HeatFluxMeasurement, ShearDeformation, NVT, NPT
+    TensileDeformation, HeatFluxMeasurement, ShearDeformation, NVT, NPT,
+    OpenMol
 ]
