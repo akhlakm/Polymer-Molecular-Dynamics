e78a73b1ba4c887864780ed9f2eaf7af11bf7374
akhlakm <me@akhlakm.com>
2023-07-30 11:11:26 -0400

    Save logs to stdout and stderr files if not quiet

--- pmd/preprocessing/amber.py
+++ pmd/preprocessing/amber.py
@@ -18,19 +18,28 @@ class AmberTool:
                     "conda install -c conda-forge ambertools ")
 
 
-    def _run_in_work_dir(self, command, quiet=False):
+    def _run_in_work_dir(self, command : str, save_prefix : str = None):
+        capture = True if save_prefix else False        
         prev_wd = os.getcwd()
+
         try:
             os.chdir(self.work_dir)
-            res = execute_command(command, capture=quiet)
+            res = execute_command(command, capture=capture)
             if res.returncode != 0:
                 raise RuntimeError(command)
+            
+            if save_prefix:
+                with open(f"{save_prefix}.stdout", "a+") as fp:
+                    fp.write(res.stdout)
+                with open(f"{save_prefix}.stderr", "a+") as fp:
+                    fp.write(res.stderr)
+
         finally:
             os.chdir(prev_wd)
 
 
     def calc_atomtypes_charges(self, input_mol2 : str, output_mol2 : str,
-                               resname : str = "POL", quiet : bool = False,
+                               resname : str = "POL", quiet : bool = True,
                                netcharge : int = 0,
                                cleanup=False):
         """ Calculate GAFF specific atom types and charges using AnteChamber."""
@@ -44,28 +53,31 @@ class AmberTool:
               f"-nc {netcharge} -pf {'y' if cleanup else 'n'}"
 
         Pmdlogging.info('Running antechamber. This may take a while ...')
-        self._run_in_work_dir(cmd, quiet)
+        saveas = "antechamber" if quiet else None
+        self._run_in_work_dir(cmd, saveas)
 
 
     def generate_ff_file(self, input_mol2 : str, output_ff_file : str,
-                         quiet : bool = False):
+                         quiet : bool = True):
         """ Generate Amber force field file from a Mol2 file using parmchk2. """
         atom_type = "gaff2"
         cmd = f"parmchk2 -i {input_mol2} -o {output_ff_file} -f mol2 " \
               f"-s {2 if atom_type == 'gaff2' else 1 } -a Y -w Y "
 
-        self._run_in_work_dir(cmd, quiet)
+        saveas = "parmchk2" if quiet else None
+        self._run_in_work_dir(cmd, saveas)
 
 
-    def build_amber_system(self, input_script : str, quiet : bool = False):
+    def build_amber_system(self, input_script : str, quiet : bool = True):
         """ Build Amber simulation files using a TLeap script. """
         cmd = f"tleap -f {input_script}"
         Pmdlogging.info('Running tleap ...')
-        self._run_in_work_dir(cmd, quiet)
+        saveas = "tleap" if quiet else None
+        self._run_in_work_dir(cmd, saveas)
 
 
     def convert_format(self, trajectory : str, output_file : str,
-                       topology : str = None, quiet : bool = False):
+                       topology : str = None, quiet : bool = True):
         """ Convert file formats using CppTraj. """
         if topology is None:
             cmd = f"cpptraj -y {trajectory} -x {output_file}"
@@ -73,4 +85,5 @@ class AmberTool:
             cmd = f"cpptraj -p {topology} -y {trajectory} -x {output_file}"
 
         Pmdlogging.info('Running cpptraj ...')
-        self._run_in_work_dir(cmd, quiet)
+        saveas = "cpptraj.convert" if quiet else None
+        self._run_in_work_dir(cmd, saveas)
